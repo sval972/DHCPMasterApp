d7ddb6fb4a0e190edf03f5424285f1264ce85201
Alexey Altoukhov <sval972@Alexeys-Mini.hsd1.wa.comcast.net>
2019-12-23 12:56:04 -0800

    Adding EULA page

---
+++ DHCPMaster/UserAgreementViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserAgreementViewController.swift
+//  DHCPMaster
+//
+//  Created by Alexey Altoukhov on 12/20/19.
+//  Copyright © 2019 Alexey Altoukhov. All rights reserved.
+//
+
+import UIKit
+
+class UserAgreementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
